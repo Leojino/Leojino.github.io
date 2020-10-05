a3ccf36ad1cc93eed859fa19bab73e4d24639e76
Jino John <leojino@gmail.com>
2020-10-05 11:54:50 +0530

    change branding

---
+++ main.js
@@ -0,0 +1,34 @@
+"use strict";
+
+
+function Main()  {
+    // Set Up Page;
+    document.title = "Jino John - Personal Space";
+    const LoaderEL = document.getElementById("loader");
+
+    init();
+
+    async function init() {
+        // remove loader
+        // document.body.classList.remove("loading");
+        await fadeOutLoader()
+    }
+
+    function fadeOutLoader() {
+        return new Promise( (resolve, reject) => {
+            const onLoaderFadeOutDone = () => {
+                LoaderEL.removeEventListener("animationend", onLoaderFadeOutDone);
+                LoaderEL.style.display = "none";
+                LoaderEL.style.animation = "";
+                resolve();
+            }
+            LoaderEL.addEventListener("animationend", onLoaderFadeOutDone);
+            LoaderEL.style.animation = "loader-fadeout 1s forwards";
+        } )
+    }
+
+}
+
+
+window.addEventListener("load", Main);
+
