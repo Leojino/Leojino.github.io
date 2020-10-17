a21f8885bc7a54ece5425d6516aaf027f4b973d9
Jino John <leojino@gmail.com>
2020-10-17 19:21:42 +0530

    change design

--- main.js
+++ main.js
@@ -3,97 +3,47 @@
 function Main() {
   // Set Up Page;
   document.title = "Jino John - Personal Space";
-  const LoaderEL = document.getElementById("loader");
+  document.body.classList.remove("loading");
 
-  init();
+  this.header = document.getElementById("header");
+  this.header.style.height = `${window.innerHeight}px`;
+  onDocumentScroll.call(this);
 
-  function init() {
-    // Start Loader
-    // await animateLoader();
-    PlayLoader();
-  }
+  bounceAppear("brand-name")
 
-  async function PlayLoader() {
-    await bounceAppear("loader-brand");
-    fadeOutLoader();
-  }
+  document.addEventListener("scroll", onDocumentScroll.bind(this));
+}
 
-  function bounceAppear(blockId) {
-    return new Promise((resolve, reject) => {
-      const Block = document.getElementById(blockId);
-      const blockSpans = Block.getElementsByTagName("span");
-      const spans = Array.from(blockSpans);
+function onDocumentScroll(e) {
+  const height = window.innerHeight - window.scrollY;
+  this.header.style.height = `${height > 60 ? height : 60}px`;
+}
 
-      spans.map((span, index) => {
-        span.style.opacity = "";
-        function endAnime(e) {
-          e.stopPropagation();
-          span.removeEventListener("animationend", endAnime);
-          span.style.opacity = "1";
-          span.style.animationName = "";
+function bounceAppear(blockId) {
+  return new Promise((resolve, reject) => {
+    const Block = document.getElementById(blockId);
+    const blockSpans = Block.getElementsByTagName("span");
+    const spans = Array.from(blockSpans);
 
-          if (spans.length == index + 1) {
-            resolve();
-          }
-        }
-        span.addEventListener("animationend", endAnime);
-        span.style.animationName = "BounceAppear";
-      });
-    });
-  }
+    spans.map((span, index) => {
+      span.style.opacity = "";
+      function endAnime(e) {
+        e.stopPropagation();
+        span.removeEventListener("animationend", endAnime);
+        span.style.opacity = "1";
+        span.style.animationName = "";
 
-  function fadeOutLoader() {
-    function onLf(e) {
-      LoaderEL.removeEventListener("animationend", onLf);
-      //   document.body.classList.remove("loading");
-      LoaderEL.classList.add("out");
-      LoaderEL.style.animation = "";
-    }
-    LoaderEL.addEventListener("animationend", onLf);
-    // LoaderEL.style.animationDuration = "2s";
-    LoaderEL.style.animationName = "loader-fadeout";
-  }
-
-  function animateLoader() {
-    return new Promise((resolve, reject) => {
-      // Appear Brand
-      const onFinalTextAppear = (el, index) => () => {
-        el.style.opacity = "1px";
-        el.removeEventListener("animationend", onFinalTextAppear);
-        if (index + 1 == Spans.length) {
+        if (spans.length == index + 1) {
           resolve();
         }
-      };
-
-      const BrandEL = LoaderEL.querySelector("#loader-brand");
-      const Spans = Array.from(BrandEL.querySelectorAll("span"));
-      Spans.map((el, index) => {
-        el.style.animationDuration = "1s";
-        el.style.animationDirection = "forwards";
-        el.style.animationName = "FadeIn";
-        el.addEventListener("animationend", onFinalTextAppear(el, index));
-      });
-    }).then(
-      () =>
-        new Promise((resolve, reject) => {
-          // FadeOut Loader screen
-          const onLoaderFadeOutDone = () => {
-            LoaderEL.removeEventListener("animationend", onLoaderFadeOutDone);
-            // LoaderEL.style.display = "none";
-            document.body.classList.remove("loading");
-            LoaderEL.style.animation = "";
-            resolve();
-          };
-          LoaderEL.addEventListener("animationend", onLoaderFadeOutDone);
-          LoaderEL.style.animation = "loader-fadeout 1s forwards";
-        })
-    );
-  }
+      }
+      span.addEventListener("animationend", endAnime);
+      span.style.animationName = "BounceAppear";
+    });
+  });
 }
 
 window.addEventListener("load", Main);
-
-console.log(document.readyState);
 
 document.addEventListener("readystatechange", (event) => {
   if (event.target.readyState === "interactive") {
